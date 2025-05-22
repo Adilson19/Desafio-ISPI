83d607caf919cc3ea68694aef30abddca8fb02a6
Adilson19 <adilsonsousaas82@gmail.com>
2025-05-22 09:35:36 +0100

    feat: criando as views

---
+++ resources/views/login.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <label>Fazer Login</label>
+    <input type="text" name="username" placeholder="Username">
+    <input type="text" name="password" placeholder="Password">
+    <button type="submit">Login</button>
+</body>
+</html>
