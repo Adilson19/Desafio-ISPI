b30773cb21bddc0881fc48061b451beb94670f77
Adilson19 <adilsonsousaas82@gmail.com>
2025-05-22 09:12:15 +0100

    feat: preenchendo os models

--- app/Models/CarOwner.php
+++ app/Models/CarOwner.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class CarOwner extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'age',
+        'bi',
+    ];
 }
