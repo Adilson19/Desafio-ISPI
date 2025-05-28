8c866fbdc411bcbf4e237bd900f92f3e6f88fe59
Adilson19 <adilsonsousaas82@gmail.com>
2025-05-28 16:42:44 +0100

    feat: actualizando a view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,17 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="">
     <title>Home</title>
 </head>
 <body>
-    
+    <header>
+        <h1>Seja Bem Vindo ao Sistema de Gestao de Oficinas</h1>
+    </header>
+    <aside></aside>
+    <section></section>
 </body>
 </html>
