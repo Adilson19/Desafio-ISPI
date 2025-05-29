23d366defe26ccf0418963fe3366ffc3227eb237
Adilson19 <adilsonsousaas82@gmail.com>
2025-05-29 16:18:39 +0100

    feat: fazendo aparecer a tela de login no navegador

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="light-style layout-menu-fixed layout-compact" dir="ltr" data-theme="theme-default" data-assets-path="{{ asset('assets') }}" data-template="vertical-menu-template-free">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="">
-    <link rel="stylesheet" href="">
     <title>Home</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <script src="{{ asset('assets/js/script.js') }}"></script>
+
 </head>
 <body>
     <header>
