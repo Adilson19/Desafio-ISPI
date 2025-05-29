66355de47f1870fd74408d653bd9b7cf0fe1bf62
Adilson19 <adilsonsousaas82@gmail.com>
2025-05-29 16:26:58 +0100

    feat: preparando o layout principal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" class="light-style layout-menu-fixed layout-compact" dir="ltr" data-theme="theme-default" data-assets-path="{{ asset('assets') }}" data-template="vertical-menu-template-free">
+<html lang="en" class="light-style layout-menu-fixed layout-compact" dir="ltr" data-theme="theme-default" data-assets-path="{{ asset('assets/css/style.css') }}" data-template="vertical-menu-template-free">
 
 <head>
     <meta charset="UTF-8">
@@ -13,6 +13,13 @@
     <header>
         <h1>Seja Bem Vindo ao Sistema de Gestao de Oficinas</h1>
     </header>
+    <label>Digite o seu nome:</label>
+        <form bindsubmit="" method="">
+            <input type="text" placeholder="Digite o seu nome" class="name-input">
+            <input type="text" placeholder="Digite o seu email" class="email-input">
+            <input type="password" placeholder="Digite a sua senha" class="password-input">
+            <button type="submit">Enviar</button>
+        </form>
     <aside></aside>
     <section></section>
 </body>
