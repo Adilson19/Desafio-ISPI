7bd8cd1ff49f51880a599ae4bb63f07736306d1a
Adilson19 <adilsonsousaas82@gmail.com>
2025-07-08 20:06:26 +0100

    feat: atualizando as views da oficina

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,106 @@
+<div id="shopify-section-footer" class="shopify-section"><footer class="footer_homepage">
+	<div class="footer_row__1">
+		<div class="container">
+			<div class="row">
+					<div class="col-sm-3 footer_block footer_block__2_1">
+
+<div class="footer_item footer_item__links">
+									<h3>Main menu</h3>
+
+									<ul>
+
+											<li>
+												<a href="https://theme642-kardone-free.myshopify.com/">Home</a>
+											</li>
+
+											<li>
+												<a href="https://theme642-kardone-free.myshopify.com/collections/all">Catalog</a>
+											</li>
+									</ul>
+								</div>
+					</div>
+					<div class="col-sm-3 footer_block ">
+
+<div class="footer_item footer_item__links">
+									<h3>Catalog</h3>
+
+									<ul>
+
+											<li>
+												<a href="https://theme642-kardone-free.myshopify.com/collections/body-parts">Body parts</a>
+											</li>
+
+											<li>
+												<a href="https://theme642-kardone-free.myshopify.com/collections/exterior">Exterior</a>
+											</li>
+
+											<li>
+												<a href="https://theme642-kardone-free.myshopify.com/collections/frontpage">Interior</a>
+											</li>
+
+											<li>
+												<a href="https://theme642-kardone-free.myshopify.com/collections/lighting">Lighting</a>
+											</li>
+
+											<li>
+												<a href="https://theme642-kardone-free.myshopify.com/collections/sale">Sale</a>
+											</li>
+
+									</ul>
+								</div>
+					</div>
+					<div class="col-sm-3 footer_block footer_block__2_1">
+
+								<div class="footer_item footer_item__contacts">
+									<h3>Our shop</h3>
+
+									<div><p><strong>Address</strong></p>
+<p>4578 Marmora St, San Francisco D04 89GR</p>
+<p><strong>Phone</strong></p>
+<p>1-800-1234-567</p></div>
+								</div>
+					</div>
+					<div class="col-sm-3 footer_block ">
+								<div class="footer_item footer_item__newsletter">
+									<h3>Newsletter</h3>
+																			<form method="post" action="https://theme642-kardone-free.myshopify.com/contact#contact_form" id="newsletter_form" accept-charset="UTF-8" class="contact-form"><input type="hidden" name="form_type" value="customer"><input type="hidden" name="utf8" value="✓">																																		<p>Join our mailing list</p>												<input type="hidden" name="contact[tags]" value="Newsletter subscriber">												<div class="form_wrapper">													<input type="email" name="contact[email]" class="input-group__field newsletter__input" placeholder="Enter your email">													<button type="submit"><i class="fa fa-arrow-right" aria-hidden="true"></i></button>												</div>
+                                                                            </form>
+								</div>
+					</div>
+						<div class="clearfix"></div>
+					<div class="col-sm-12 footer_block footer_block__2_1">
+								<div class="footer_item footer_item__social">
+									<ul>
+											<li><a href="https://www.facebook.com/TemplateMonster/"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+											<li><a href="https://twitter.com/templatemonster"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+											<li><a href="https://www.youtube.com/user/TemplateMonsterCo"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+									</ul>
+								</div>
+					</div>
+						<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+		<div class="footer_row__2">
+			<div class="container">
+				<div class="footer_wrap_1">
+						<div class="footer_wrap_2 footer_left">
+							<a target="_blank" rel="nofollow" href="https://www.shopify.com/?utm_campaign=poweredby&amp;utm_medium=shopify&amp;utm_source=onlinestore">Powered by Shopify</a>.
+                          	<span> Design by <a href="https://zemez.io/">Zemez</a></span>
+						</div>
+						<div class="footer_wrap_2 footer_right">
+							<ul class="footer_payments">
+										<li><i class="fa fa-cc-mastercard" aria-hidden="true"></i></li>
+										<li><i class="fa fa-cc-paypal" aria-hidden="true"></i></li>
+										<li><i class="fa fa-cc-visa" aria-hidden="true"></i></li>
+							</ul>
+						</div>
+
+				</div>
+			</div>
+		</div>
+</footer>
+
+</div>
+			</div>
+		</div>
