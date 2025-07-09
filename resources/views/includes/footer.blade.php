f3c589001f79bd4d18f02f9b0ae9ae4db61ce00a
Adilson19 <adilsonsousaas82@gmail.com>
2025-07-09 10:48:46 +0100

    feat: atualizando o footer e o header

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,103 +1,41 @@
-<div id="shopify-section-footer" class="shopify-section"><footer class="footer_homepage">
-	<div class="footer_row__1">
-		<div class="container">
-			<div class="row">
-					<div class="col-sm-3 footer_block footer_block__2_1">
-<div class="footer_item footer_item__links">
-									<h3>Main menu</h3>
-									<ul>
-											<li>
-												<a href="https://theme642-kardone-free.myshopify.com/">Home</a>
-											</li>
-
-											<li>
-												<a href="https://theme642-kardone-free.myshopify.com/collections/all">Catalog</a>
-											</li>
-									</ul>
-								</div>
-					</div>
-					<div class="col-sm-3 footer_block ">
-
-<div class="footer_item footer_item__links">
-									<h3>Catalog</h3>
-
-									<ul>
-
-											<li>
-												<a href="https://theme642-kardone-free.myshopify.com/collections/body-parts">Body parts</a>
-											</li>
-
-											<li>
-												<a href="https://theme642-kardone-free.myshopify.com/collections/exterior">Exterior</a>
-											</li>
-
-											<li>
-												<a href="https://theme642-kardone-free.myshopify.com/collections/frontpage">Interior</a>
-											</li>
-
-											<li>
-												<a href="https://theme642-kardone-free.myshopify.com/collections/lighting">Lighting</a>
-											</li>
-
-											<li>
-												<a href="https://theme642-kardone-free.myshopify.com/collections/sale">Sale</a>
-											</li>
-
-									</ul>
-								</div>
-					</div>
-					<div class="col-sm-3 footer_block footer_block__2_1">
-
-								<div class="footer_item footer_item__contacts">
-									<h3>Our shop</h3>
-
-									<div><p><strong>Address</strong></p>
-<p>4578 Marmora St, San Francisco D04 89GR</p>
-<p><strong>Phone</strong></p>
-<p>1-800-1234-567</p></div>
-								</div>
-					</div>
-					<div class="col-sm-3 footer_block ">
-								<div class="footer_item footer_item__newsletter">
-									<h3>Newsletter</h3>
-																			<form method="post" action="https://theme642-kardone-free.myshopify.com/contact#contact_form" id="newsletter_form" accept-charset="UTF-8" class="contact-form"><input type="hidden" name="form_type" value="customer"><input type="hidden" name="utf8" value="✓">																																		<p>Join our mailing list</p>												<input type="hidden" name="contact[tags]" value="Newsletter subscriber">												<div class="form_wrapper">													<input type="email" name="contact[email]" class="input-group__field newsletter__input" placeholder="Enter your email">													<button type="submit"><i class="fa fa-arrow-right" aria-hidden="true"></i></button>												</div>
-                                                                            </form>
-								</div>
-					</div>
-						<div class="clearfix"></div>
-					<div class="col-sm-12 footer_block footer_block__2_1">
-								<div class="footer_item footer_item__social">
-									<ul>
-											<li><a href="https://www.facebook.com/TemplateMonster/"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-											<li><a href="https://twitter.com/templatemonster"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-											<li><a href="https://www.youtube.com/user/TemplateMonsterCo"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
-									</ul>
-								</div>
-					</div>
-						<div class="clearfix"></div>
-			</div>
-		</div>
-	</div>
-		<div class="footer_row__2">
-			<div class="container">
-				<div class="footer_wrap_1">
-						<div class="footer_wrap_2 footer_left">
-							<a target="_blank" rel="nofollow" href="https://www.shopify.com/?utm_campaign=poweredby&amp;utm_medium=shopify&amp;utm_source=onlinestore">Powered by Shopify</a>.
-                          	<span> Design by <a href="https://zemez.io/">Zemez</a></span>
-						</div>
-						<div class="footer_wrap_2 footer_right">
-							<ul class="footer_payments">
-										<li><i class="fa fa-cc-mastercard" aria-hidden="true"></i></li>
-										<li><i class="fa fa-cc-paypal" aria-hidden="true"></i></li>
-										<li><i class="fa fa-cc-visa" aria-hidden="true"></i></li>
-							</ul>
-						</div>
-
-				</div>
-			</div>
-		</div>
-</footer>
-
-</div>
-			</div>
-		</div>
+<footer class="bg-dark text-white mt-5">
+        <div class="container py-4">
+            <div class="row">
+                <div class="col-md-3 col-6">
+                    <h4 class="h6">PÁGINAS</h4>
+                    <ul class="list-unstyled"><!--Tirando o estilo-->
+                        <li><a href="planos.html">Planos</a></li>
+                        <li><a href="contato.html">Contato</a></li>
+                        <li><a href="inscricao.html">Inscreva-se</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-6">
+                    <h4 class="h6">LOCAIS</h4>
+                    <ul class="list-unstyled"><!--Tirando o estilo-->
+                        <li><a href="local.html">California</a></li>
+                        <li><a href="local.html">Paris</a></li>
+                        <li><a href="local.html">Dublin</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h4 class="h6">DADOS DE CONTATO</h4>
+                    <ul class="list-unstyled text-secondary"><!--Tirando o estilo-->
+                        <li>adilsonsousaas82@gmail.com</li>
+                        <li>+244 922 427 380</li>
+                        <li>De seg. à Sex. das 8h às 18h</li>
+                    </ul>
+                </div>
+                <div class="col-md-2">
+                    <h4 class="h6">REDES SOCIAIS</h4>
+                    <ul class="list-unstyled"><!--Tirando o estilo-->
+                        <li><a style="max-width: 140px;" class="btn btn-sm btn-outline-secondary btn-block mb-2" href="#">Facebook</a></li>
+                        <li><a style="max-width: 140px;" class="btn btn-sm btn-outline-secondary btn-block mb-2" href="#">Twitter</a></li>
+                        <li><a style="max-width: 140px;" class="btn btn-sm btn-outline-secondary btn-block mb-2" href="#">Youtube</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="bg-primary text-center py-3">
+            <p class="mb-0">Adelino Sousa © 2025. Alguns direitos reservados.</p>
+        </div>
+    </footer>
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,144 +1,43 @@
-<div id="sticky_header" class="header_row__2 megamenu_stuck">
-		<div class="header_row__2_1 stick_banner" style="background-image: url( //theme642-kardone-free.myshopify.com/cdn/shop/t/2/assets/kardone_top_banner.png?v=56600348324023128651553065130 );">
-			<div class="banner_text">
-				<h3><b>Upgrade</b> to a Fully-fledged <b>Kardone!</b></h3>
-				<h4>Featuring a additional pages, plugins, beautiful pictures and fully functionality!</h4>
-				<a href="https://www.templatemonster.com/shopify-themes/auto-parts-responsive-shopify-theme-62439.html" class="btn">SHOP NOW!</a>
-		</div>
-		</div>
-		<div class="page_container">
-			<div class="container">
-				<h1>
-								<a class="header_logo" href="https://theme642-kardone-free.myshopify.com/">
-									<img src="./Kardone_files/logo_x40.png" alt="Kardone">
-								</a>
-				</h1>
-					<nav id="megamenu" class="megamenu megamenu_desktop">
-						<h2 id="megamenu_mobile_toggle" title="Menu"><i class="fa fa-bars" aria-hidden="true"></i></h2>
-						<div id="megamenu_mobile_close">
-							<div class="close_icon"></div>
-						</div>
-						<ul id="megamenu_level__1" class="level_1 sf-js-enabled sf-arrows" style="">
-								<li class="level_1__item">
-									<a class="level_1__link active" href="https://theme642-kardone-free.myshopify.com/">Home
-									</a>
-								</li>
-								<li class="level_1__item level_2__links">
-									<a class="level_1__link sf-with-ul" href="https://theme642-kardone-free.myshopify.com/collections">Catalog
-										<i class="level_1__trigger megamenu_trigger" data-submenu="menu_1479745635495"></i>
-									</a>
-
-
-
-												<ul class="level_2" id="menu_1479745635495" style="display: none;">
-													<li class="container">
-
-
-<div class="megamenu_col__item">
-																<h3>Catalog <i class="level_2__trigger megamenu_trigger" data-submenu="menu_1479745635495-1"></i></h3>
-
-																<ul class="level_3" id="menu_1479745635495-1">
-
-																		<li class="level_3__item">
-																			<a class="level_3__link" href="https://theme642-kardone-free.myshopify.com/collections/body-parts">Body parts</a>
-																		</li>
-
-																		<li class="level_3__item">
-																			<a class="level_3__link" href="https://theme642-kardone-free.myshopify.com/collections/exterior">Exterior</a>
-																		</li>
-
-																		<li class="level_3__item">
-																			<a class="level_3__link" href="https://theme642-kardone-free.myshopify.com/collections/frontpage">Interior</a>
-																		</li>
-
-																		<li class="level_3__item">
-																			<a class="level_3__link" href="https://theme642-kardone-free.myshopify.com/collections/lighting">Lighting</a>
-																		</li>
-
-																		<li class="level_3__item">
-																			<a class="level_3__link" href="https://theme642-kardone-free.myshopify.com/collections/sale">Sale</a>
-																		</li>
-
-																</ul>
-															</div>
-
-
-<div class="megamenu_col__item">
-																<h3>Catalog <i class="level_2__trigger megamenu_trigger" data-submenu="menu_1479745635495-2"></i></h3>
-
-																<ul class="level_3" id="menu_1479745635495-2">
-
-																		<li class="level_3__item">
-																			<a class="level_3__link" href="https://theme642-kardone-free.myshopify.com/collections/body-parts">Body parts</a>
-																		</li>
-
-																		<li class="level_3__item">
-																			<a class="level_3__link" href="https://theme642-kardone-free.myshopify.com/collections/exterior">Exterior</a>
-																		</li>
-
-																		<li class="level_3__item">
-																			<a class="level_3__link" href="https://theme642-kardone-free.myshopify.com/collections/frontpage">Interior</a>
-																		</li>
-
-																		<li class="level_3__item">
-																			<a class="level_3__link" href="https://theme642-kardone-free.myshopify.com/collections/lighting">Lighting</a>
-																		</li>
-
-																		<li class="level_3__item">
-																			<a class="level_3__link" href="https://theme642-kardone-free.myshopify.com/collections/sale">Sale</a>
-																		</li>
-
-																</ul>
-															</div>
-
-
-<div class="megamenu_col__item">
-																<h3>Catalog <i class="level_2__trigger megamenu_trigger" data-submenu="menu_1479745635495-3"></i></h3>
-
-																<ul class="level_3" id="menu_1479745635495-3">
-
-																		<li class="level_3__item">
-																			<a class="level_3__link" href="https://theme642-kardone-free.myshopify.com/collections/body-parts">Body parts</a>
-																		</li>
-
-																		<li class="level_3__item">
-																			<a class="level_3__link" href="https://theme642-kardone-free.myshopify.com/collections/exterior">Exterior</a>
-																		</li>
-
-																		<li class="level_3__item">
-																			<a class="level_3__link" href="https://theme642-kardone-free.myshopify.com/collections/frontpage">Interior</a>
-																		</li>
-
-																		<li class="level_3__item">
-																			<a class="level_3__link" href="https://theme642-kardone-free.myshopify.com/collections/lighting">Lighting</a>
-																		</li>
-
-																		<li class="level_3__item">
-																			<a class="level_3__link" href="https://theme642-kardone-free.myshopify.com/collections/sale">Sale</a>
-																		</li>
-																</ul>
-															</div>
-<div class="megamenu_col__item">
-																<h3>Catalog <i class="level_2__trigger megamenu_trigger" data-submenu="menu_1479745635495-4"></i></h3>
-
-																<ul class="level_3" id="menu_1479745635495-4">
-																		<li class="level_3__item">
-																			<a class="level_3__link" href="https://theme642-kardone-free.myshopify.com/collections/body-parts">Body parts</a>
-																		</li>
-																		<li class="level_3__item">
-																			<a class="level_3__link" href="https://theme642-kardone-free.myshopify.com/collections/exterior">Exterior</a>
-																		</li>
-																		<li class="level_3__item">
-																			<a class="level_3__link" href="https://theme642-kardone-free.myshopify.com/collections/frontpage">Interior</a>
-																		</li>
-																		<li class="level_3__item">
-																			<a class="level_3__link" href="https://theme642-kardone-free.myshopify.com/collections/lighting">Lighting</a>
-																		</li>
-																		<li class="level_3__item">
-																			<a class="level_3__link" href="https://theme642-kardone-free.myshopify.com/collections/sale">Sale</a>
-																		</li>
-																</ul>
-															</div>
-													</li>
-												</ul>
-								</li>
+<!--Barra de navegacao-->
+    <nav class="navbar navbar-expand-md navbar-light bg-light fixed-top py-3 box-shadow">
+        <!--Link da imagem do logo do caravan-->
+        <a href="index.html" class="navbar-brand">
+            <!--A imagem-->
+            <img src="img/caravan.svg" alt="Caravan">
+        </a>
+        <!--Botao de menu-->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Abrir Navegação">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- Tudo o que estiver nessa div vamos esconder -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!--Lista de links da navegacao-->
+            <!--Margem a esquerda sera auto e a direita fixa-->
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item dropdown">
+                    <!--Link do botao que vai encolher-se ao longo da exibicao-->
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Locais</a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="local.html">California</a>
+                        <a class="dropdown-item" href="local.html">Paris</a>
+                        <a class="dropdown-item" href="local.html">Dublin</a>
+                    </div>
+                </li>
+                <!--Elementos do menu-->
+                <li class="nav-item">
+                    <a class="nav-link" href="planos.html">Planos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="contato.html">Contato</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" data-toggle="modal" data-target="#modalLogin">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn btn-outline-primary ml-md-2" href="inscricao.html">Inscreva-se</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
