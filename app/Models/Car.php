8efd1a1c968d0e7e0c4f04b8c4d5fe204be0d737
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-21 22:10:55 +0100

    feat: update

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Models\Car_owners;
 class Car extends Model
 {
     use HasFactory;
