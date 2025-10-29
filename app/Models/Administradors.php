09c823e225a4caa524a27c9a22b0b94d6a35f1d7
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-29 09:30:38 +0100

    feat: update

--- app/Models/Administradors.php
+++ app/Models/Administradors.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Administradors extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'nivel'
+    ];
 }
