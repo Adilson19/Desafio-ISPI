66fb055825495333a58ddef6b91eeb2dd6036db3
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-10-29 10:16:02 +0100

    feat: update view

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,15 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/assets/css/style.css">
     <title>Login</title>
 </head>
 <body>
-    <label>Fazer Login</label>
+    <h1>Fazer Login</h1>
     <form bindsubmit="">
-        <label>Nome do usuario</label>
+        <label>Nome do usuario: </label><br>
         <input type="text" name="username" placeholder="Username"><br/>
-        <label>Palavra passe</label>
-        <input type="text" name="password" placeholder="Password"><br/>
+        <label>Palavra passe: </label><br>
+        <input type="text" name="password" placeholder="Password"><br/><br>
         <button type="submit">Login</button>
     </form>
 </body>
