83f83d3aca85c8abe57213238ab302c134e7d2d0
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-11-07 15:13:39 +0100

    feat: creating produtos model and migration

---
+++ app/Models/Produto.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produto extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'id_produto',
+        'nome_produto',
+        'descricao',
+        'valor'
+    ];
+}
---
+++ database/migrations/2025_11_07_140328_create_produtos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_produto')->nullable()->unique();
+            $table->integer('unidade_medida');
+            $table->string('descricao');
+            $table->double('valor');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
