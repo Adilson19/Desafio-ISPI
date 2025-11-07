49243acb454edb5b85e502ecaf0dba7d143c8e39
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-11-07 15:02:58 +0100

    feat: creting two models and migrations

---
+++ database/migrations/2025_11_07_135540_create_cidades_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cidades', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_cidade')->unique();
+            $table->string('nome_cidade');
+            $table->string('UF');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cidades');
+    }
+};
